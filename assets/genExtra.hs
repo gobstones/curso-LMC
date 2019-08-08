231f766af2fa6b76333a55526f323bcadd48ec79
fidel-ml <fidel@unq.edu.ar>
2019-08-08 15:32:21 -0300

    Animation complete (an example is needed)

--- assets/genExtra.hs
+++ assets/genExtra.hs
@@ -1,6 +1,7 @@
 import System.Directory
 
-filesToImport = [ "License"          , "LMCOps-BoardDraw"   , "LMCOps-BoardRead"
+filesToImport = [ "License"          
+                , "LMCOps-BoardDraw"   , "LMCOps-BoardRead" , "LMCOps-BoardExecution"
                 , "LMCDef-Programs"  , "LMCDef-Full"        , "LMCDef-Form"      
                 , "LMCDef-F-BoardRep", "LMCDef-F-BoardPlace", "LMCDef-F-Model", "LMCDef-FM-Mem"    
                 , "GobstonesLists"   , "GobstonesAmenities"  
